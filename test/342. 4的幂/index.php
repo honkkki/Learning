d5bdb7c39175de09db09127944291cc70a017472
caiminfang <244491216@qq.com>
2020-04-13 14:12:15 +0800

    4的幂

---
+++ test/342. 4的幂/index.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+342. 4的幂
+给定一个整数 (32 位有符号整数)，请编写一个函数来判断它是否是 4 的幂次方。
+
+示例 1:
+
+输入: 16
+输出: true
+示例 2:
+
+输入: 5
+输出: false
+ */
+class Solution {
+
+    /**
+     * ($num & ($num - 1)) == 0 判断是否被2整除，参考231题
+     */
+    function isPowerOfFour($num) {
+        return $num > 0 && ($num & ($num - 1)) == 0 && $num % 3 == 1;
+    }
+
+    //被4整除
+    function isPowerOfFou1r($num) {
+        if ($num == 1 OR ($num > 1 && $num % 4 == 0)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
+
+$solution = new Solution();
+var_dump($solution->isPowerOfFour(16));
